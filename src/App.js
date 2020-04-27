bb0cbbb96758015754298d170db2dc6e34674a77
ben004 <benbeniel007@gmail.com>
2020-04-27 19:19:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,74 +1,83 @@
-import React, {Component} from 'react'
-import axios from 'axios'
-import ReactPaginate from 'react-paginate';
-import './App.css'
+import React, { Component } from "react";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
+import "./App.css";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            offset: 0,
-            data: [],
-            perPage: 10,
-            currentPage: 0
-        };
-        this.handlePageClick = this
-            .handlePageClick
-            .bind(this);
-    }
-    receivedData() {
-        axios
-            .get(`https://jsonplaceholder.typicode.com/photos`)
-            .then(res => {
-
-                const data = res.data;
-                const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
-                const postData = slice.map(pd => <React.Fragment>
-                    <p>{pd.title}</p>
-                    <img src={pd.thumbnailUrl} alt=""/>
-                </React.Fragment>)
-
-                this.setState({
-                    pageCount: Math.ceil(data.length / this.state.perPage),
-                   
-                    postData
-                })
-            });
-    }
-    handlePageClick = (e) => {
-        const selectedPage = e.selected;
-        const offset = selectedPage * this.state.perPage;
-
-        this.setState({
-            currentPage: selectedPage,
-            offset: offset
-        }, () => {
-            this.receivedData()
-        });
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      offset: 0,
+      data: [],
+      perPage: 10,
+      currentPage: 0,
     };
+  }
+  receivedData() {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`).then((res) => {
+      const data = res.data;
+      const slice = data.slice(
+        this.state.offset,
+        this.state.offset + this.state.perPage
+      );
+      const postData = slice.map((pd) => (
+          <p>{pd.title}</p>
+      ));
 
-    componentDidMount() {
-        this.receivedData()
-    }
-    render() {
-        return (
-            <div>
-                {this.state.postData}
-                <ReactPaginate
-                    previousLabel={"prev"}
-                    nextLabel={"next"}
-                    breakLabel={"..."}
-                    breakClassName={"break-me"}
-                    pageCount={this.state.pageCount}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={5}
-                    onPageChange={this.handlePageClick}
-                    containerClassName={"pagination"}
-                    subContainerClassName={"pages pagination"}
-                    activeClassName={"active"}/>
-            </div>
+      this.setState({
+        pageCount: Math.ceil(data.length / this.state.perPage),
+        postData,
+      });
+    });
+  }
+  handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    const offset = selectedPage * this.state.perPage;
+    this.setState(
+      {
+        currentPage: selectedPage,
+        offset: offset,
+      },
+      () => {
+        this.receivedData();
+      }
+    );
+  };
 
-        )
-    }
+  componentDidMount() {
+    this.receivedData();
+  }
+  handlechange(e) {
+    this.setState({
+      perPage: e.target.value,
+    },()=>{
+        console.log(this.state.perPage)//check value updateed or not
+    });
+  }
+  render() {
+    return (
+      <div>
+        <select onClick={(e) => this.handlechange(e)}>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="25">25</option>
+        </select>
+        {this.state.postData}
+        <ReactPaginate
+          previousLabel={"prev"}
+          nextLabel={"next"}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={this.state.pageCount}
+          itemsCountPerPage={this.state.perPage}
+          marginPagesDisplayed={10}
+          pageRangeDisplayed={1}
+          onPageChange={(e) => this.handlePageClick(e)}
+          containerClassName={"pagination"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"}
+        />
+      </div>
+    );
+  }
 }
